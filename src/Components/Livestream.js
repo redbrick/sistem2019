e914950d3433fcd906d06415d3d011433c2aafb8
Sean Fradl (Fraz) <seanfradl@gmail.com>
2019-02-13 20:17:51 +0000

    Update Livestream.js
    Commented out Rick Astley example video stream...

--- src/Components/Livestream.js
+++ src/Components/Livestream.js
@@ -12,7 +12,7 @@ class Livestream extends Component {
             let livestreamEmbedCode = [];
             livestreamEmbedCode.push(
                 /* Replace me with an iframe! */
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                /* <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
                 
             )
             if(livestreamEmbedCode.length === 0){
