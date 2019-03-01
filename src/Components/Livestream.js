b055cea31cfaa20b4df8b70a92308142b330a55e
Ben McMahon <benmcmahon100:gmail.com>
2019-03-01 17:32:10 +0000

    Add Stream:w

--- src/Components/Livestream.js
+++ src/Components/Livestream.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Livestream = () => { 
-   const liveStreamEmbed = () => {
+   const Stream = () => {
       /* Enter livestream link below! */
       let livestreamEmbedCode = [];
       livestreamEmbedCode.push(
          /* Replace me with an iframe! */
          /* <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
+         <iframe key = "0" title = "Sistem 2019 | Live" src="https://media.heanet.ie/player/live/484ec29e934b8396d5d8de5880b98f1d" id="484ec29e934b8396d5d8de5880b98f1d" width="640" height="360" marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" allowFullScreen></iframe>
       )
       
       if(livestreamEmbedCode.length === 0){
@@ -14,7 +15,7 @@ const Livestream = () => {
             <p>The livestream will be available to watch here on the day of the event. Please come back then to watch.</p>
          )
       }
-      console.log(livestreamEmbedCode)
+      
       return livestreamEmbedCode
    }
 
@@ -29,8 +30,7 @@ const Livestream = () => {
             </div>
             <div className="content-container">
                <div className="page">
-                  <h3>Watch Now</h3>
-                  <liveStreamEmbed />
+                  <Stream />
                </div>
             </div>
       </div>
--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -8,17 +8,17 @@ class SocialMedia extends Component {
     render() {
         return (
             <div className="menu-container">
-                <span class="fa-stack fa-lg icons">
+                <span className="fa-stack fa-lg icons">
                 <a href="https://www.facebook.com/share.php?u=https://sistem.redbrick.dcu.ie/&title=SISTEM">
-                <i class="fa fa-circle fa-stack-2x" style={{color: '#272324'}}></i>
-                <i class="fab fa-facebook-f fa-stack-1x"></i>
+                <i className="fa fa-circle fa-stack-2x" style={{color: '#272324'}}></i>
+                <i className="fab fa-facebook-f fa-stack-1x"></i>
                 </a>
                 </span>
 
-                <span class="fa-stack fa-lg icons">
+                <span className="fa-stack fa-lg icons">
                 <a href="https://twitter.com/intent/tweet?status=SISTEM+https://sistem.redbrick.dcu.ie/">
-                <i class="fa fa-circle fa-stack-2x" style={{color: '#272324'}}></i>
-                <i class="fab fa-twitter fa-stack-1x"></i>
+                <i className="fa fa-circle fa-stack-2x" style={{color: '#272324'}}></i>
+                <i className="fab fa-twitter fa-stack-1x"></i>
                 </a>
                 </span>
 
